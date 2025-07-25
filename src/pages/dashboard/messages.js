92ae94a48a2d069ded64df87ed49022e38b288ef
Abdullah Oyedele <olawaleoyedele@gmail.com>
2025-07-25 18:25:33 +0100

    bug fix

---
+++ src/pages/dashboard/messages.js
@@ -0,0 +1,12 @@
+// pages/dashboard/messages.js
+
+import React from 'react';
+
+export default function Messages() {
+  return (
+    <div>
+      <h1>Messages</h1>
+      <p>This is the messages page.</p>
+    </div>
+  );
+}
